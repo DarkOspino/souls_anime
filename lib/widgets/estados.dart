42a678a2fdbfa9f0fc13f4bcd50c954014f1671d
DarkOspino <92461938+DarkOspino@users.noreply.github.com>
2021-12-09 18:17:21 -0600

    Add files via upload

---
+++ lib/widgets/estados.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Estados extends StatelessWidget {
+  const Estados({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Estados de Anime"));
+  }
+}
